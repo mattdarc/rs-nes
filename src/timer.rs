dde885c849a043565ceb178aa16150abec323daa
Matt D'Arcangelo <matthew.darcangelo@pm.me>
2023-11-20 12:23:40 -0500

    Improve timers
    Instead of the timers eagerly pushing to a centralized location, share
    the state used internally with a global registry that is populated on
    static initialization. This allows each timer to cache their own
    reference locally and update it without requiring a HashMap lookup.
    
    Change the entries to a Vec<TimeResult> to handle the case where
    multiple timers are registered to the same identifier. The results are
    collated before being reported.

--- src/timer.rs
+++ src/timer.rs
@@ -1,37 +1,26 @@
-use std::cell::UnsafeCell;
-use std::collections::{hash_map::RawEntryMut, HashMap};
-use std::hash::{BuildHasher, DefaultHasher, Hash, Hasher};
+use std::cell::{RefCell, UnsafeCell};
+use std::collections::HashMap;
+use std::rc::Rc;
 pub use std::time::Duration;
-
-#[derive(Default)]
-struct UnusedHasher;
-
-impl BuildHasher for UnusedHasher {
-    type Hasher = DefaultHasher;
-
-    fn build_hasher(&self) -> Self::Hasher {
-        DefaultHasher::default()
-    }
-
-    fn hash_one<T: Hash>(&self, _x: T) -> u64 {
-        panic!("This hasher is unused and should not be called. This may have occurred during a rehash")
-    }
-}
-
-thread_local! {
-    static LOCAL_REGISTRY: UnsafeCell<TimeResultRegistry> = UnsafeCell::new(TimeResultRegistry::default());
-}
 
 #[cfg(not(feature = "notimers"))]
 macro_rules! timed {
     ($name:literal, $contents:block) => {{
-        use lazy_static::lazy_static;
-        lazy_static! {
-            static ref TIMER: timer::TimerName = timer::TimerName::new($name);
+        use std::cell::UnsafeCell;
+
+        thread_local! {
+            static TIMER: UnsafeCell<timer::Timer> = UnsafeCell::new(timer::Timer::new($name));
         }
 
-        let _scoped_timer = timer::ScopedTimer::new(&TIMER);
-        $contents
+        // Both are safe as
+        //   a) This timer is only referenced in this scope, so there can be no references outside
+        //      of this scope.
+        //   b) No references leave the thread-local scope
+        TIMER.with(|t| unsafe { (*t.get()).start() });
+        let ret = $contents;
+        TIMER.with(|t| unsafe { (*t.get()).stop() });
+
+        ret
     }};
 }
 
@@ -65,92 +54,79 @@ impl FastInstant {
     }
 }
 
-#[derive(Eq)]
-pub struct TimerName(&'static str, u64);
-
-impl TimerName {
-    pub fn new(name: &'static str) -> Self {
-        let mut hasher = DefaultHasher::default();
-        name.hash(&mut hasher);
-        let hash = hasher.finish();
-
-        TimerName(name, hash)
-    }
-}
-
-impl PartialEq for TimerName {
-    fn eq(&self, other: &Self) -> bool {
-        let same = self.0.as_ptr() == other.0.as_ptr();
-        assert!(!same || self.1 == other.1, "Found entries with bad hashes");
-
-        same
-    }
-}
-
 // Registry of time results across the whole program. These are written to disk or printed on
 // thread exit, since we do not to do this while anything is running
 struct TimeResultRegistry {
     global_start: FastInstant,
-    results: HashMap<&'static str, TimeResult, UnusedHasher>,
+    results: HashMap<&'static str, Vec<Rc<RefCell<TimeResult>>>>,
 }
 
 impl Default for TimeResultRegistry {
     fn default() -> Self {
-        // Arbitrary number of timers allowed to prevent rehashes and allocations
-        const NUM_TIMERS: usize = 64;
-
         TimeResultRegistry {
             global_start: FastInstant::now(),
-            results: HashMap::with_capacity_and_hasher(64, UnusedHasher::default()),
+            results: HashMap::new(),
         }
     }
 }
 
-// Name: duration mapping for serializing to disk or printing
+// Name: Duration mapping for serializing to disk or printing
 #[derive(Default)]
 struct TimeResult {
     total_duration: Duration,
     samples: u64,
 }
 
-pub struct ScopedTimer {
-    name: &'static TimerName,
+pub struct Timer {
+    name: &'static str,
     start: FastInstant,
+    result: Rc<RefCell<TimeResult>>,
 }
 
-impl ScopedTimer {
-    pub fn new(name: &'static TimerName) -> Self {
-        ScopedTimer {
+impl Timer {
+    pub fn new(name: &'static str) -> Self {
+        let result = Rc::new(RefCell::new(TimeResult::default()));
+
+        TimeResultRegistry::add_timer(name, result.clone());
+
+        Timer {
             name,
+            result,
             start: FastInstant::now(),
         }
     }
-}
 
-impl Drop for ScopedTimer {
-    fn drop(&mut self) {
-        TimeResultRegistry::add(self.name, self.start.elapsed())
+    pub fn start(&mut self) {
+        self.start = FastInstant::now();
+    }
+
+    pub fn stop(&mut self) {
+        let elapsed = self.start.elapsed();
+
+        let mut result = self.result.borrow_mut();
+        result.total_duration += elapsed;
+        result.samples += 1;
     }
 }
 
 impl TimeResultRegistry {
-    fn add(timer: &TimerName, duration: Duration) {
-        LOCAL_REGISTRY.with(|r| {
-            // SAFETY: This is safe since we're operating on a thread-local and this is the only way
-            // for it to be mutated
-            let raw_entry = unsafe { (*r.get()).results.raw_entry_mut() };
+    fn add_timer(name: &'static str, result: Rc<RefCell<TimeResult>>) {
+        thread_local! {
+            static LOCAL_REGISTRY: UnsafeCell<TimeResultRegistry> = (|| {
+                UnsafeCell::new(TimeResultRegistry::default())
+            })();
+        }
 
-            let &TimerName(name, hash) = timer;
-            let entry = match raw_entry.from_key_hashed_nocheck(hash, name) {
-                RawEntryMut::Occupied(entry) => entry.into_mut(),
-                RawEntryMut::Vacant(entry) => {
-                    entry
-                        .insert_hashed_nocheck(hash, name, TimeResult::default())
-                        .1
-                }
-            };
-            entry.total_duration += duration;
-            entry.samples += 1;
+        LOCAL_REGISTRY.with(|r| unsafe {
+            // SAFETY:
+            //
+            // Safe as we're operating on a thread-local and this is the only way for it to be
+            // mutated or referenced
+            (*r.get())
+                .results
+                .entry(name)
+                .or_insert_with(|| Vec::new())
+                .push(result);
         });
     }
 }
@@ -160,28 +136,33 @@ impl Drop for TimeResultRegistry {
         let global_duration = self.global_start.elapsed().as_micros() as f64;
         let global_duration_div = global_duration / 100.;
 
-        println!("\nTiming (total: {} us)", global_duration);
-
-        let mut sorted_results: Vec<_> = self.results.iter().collect();
+        let mut sorted_results: Vec<_> = self
+            .results
+            .iter()
+            .map(|(name, times)| {
+                (
+                    name,
+                    times.iter().fold(TimeResult::default(), |a, b| TimeResult {
+                        total_duration: a.total_duration + b.borrow().total_duration,
+                        samples: a.samples + b.borrow().samples,
+                    }),
+                )
+            })
+            .collect();
         sorted_results.sort_by(|(_, a), (_, b)| b.total_duration.cmp(&a.total_duration));
 
-        for (
-            name,
-            TimeResult {
-                total_duration,
-                samples,
-            },
-        ) in &sorted_results
-        {
-            let dur = total_duration.as_micros() as f64;
+        println!("\nTiming (total: {} us)", global_duration);
+        for (name, result) in &sorted_results {
+            let dur = result.total_duration.as_micros() as f64;
             println!(
                 "  {:<30}: {:>10} us ({:>5.2}%), avg: {:>10.02} us, samples {:>10}",
                 name,
                 dur,
                 dur / global_duration_div,
-                dur / (*samples as f64),
-                samples,
+                dur / (result.samples as f64),
+                result.samples,
             );
         }
+        println!();
     }
 }
